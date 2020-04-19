0e094f546fecaeadff9a781a254a8e52a56ed9d0
Vedant Khairnar <vedron007@gmail.com>
2020-04-19 13:51:31 +0530

    Code 2.0

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
 from flask import Flask,render_template,request,redirect
-import source as sr
+import sourceTemp as sr
+import casedata as cd
 
 app = Flask(__name__)
+
+
+@app.route('/home')
+def Newsify_DashBoard():
+    totalconfirmed=cd.current_data["totalconfirmed"]
+    totaldeceased=cd.current_data["totaldeceased"]
+    totalrecovered=cd.current_data["totalrecovered"]
+    dailyconfirmed=cd.current_data["dailyconfirmed"]
+    dailydeceased = cd.current_data["dailydeceased"]
+    dailyrecovered = cd.current_data["dailyrecovered"]
+    return render_template('home.html',totalconfirmed=totalconfirmed,totaldeceased=totaldeceased,totalrecovered=totalrecovered,dailyconfirmed=dailyconfirmed,dailydeceased=dailydeceased,dailyrecovered=dailyrecovered)
 
 @app.route('/')
 def redirection():
     return redirect('/home')
 
-
-@app.route('/home')
-def Newsify_DashBoard():
-    return render_template('home.html')
-
-
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == "POST":
         newsTitle=request.form['newsTitle']
-        keywords=request.form['keywords']
 
-        a=sr.supreme(keywords,newsTitle)
+
+        a=sr.supreme(newsTitle)
         truth=a["truthfulness"]
         links=a["links"]
         intersection=a["intersection"]
+        latest_news=a["latest_news"]
 
 
-    return render_template('result.html', truth = truth,intersection=intersection,links=links)
+
+    return render_template('result.html', truth = truth,intersection=intersection,links=links,latest_news=latest_news)
 
 
 if __name__ == '__main__':
